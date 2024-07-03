7b250afefe7a78c866a0f87ab2efd25ccf6ea1da
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-07-03 12:43:02 +0100

    Connectiont to rabbitmq establised

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connString := "amqp://guest:guest@localhost:5672/"
+	cenas, err := amqp.Dial(connString)
+	defer cenas.Close()
+	if err != nil {
+		fmt.Println("connection not done")
+	}
+
+	fmt.Println("Connection Successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	shuttingDown := <-signalChan
+	if shuttingDown != nil {
+		fmt.Println("Program is shutting down.")
+	}
+
 }
