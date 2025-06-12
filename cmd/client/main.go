0fd29beaeee83f6a2daab4ecb38b76e7f27a3f18
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-06-12 12:03:40 +0100

    starting peril client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
 }
